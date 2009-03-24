b32a81ffc5ed82fe6847f1fbdbb48ccf10203300
estesox <estesox@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-03-24 21:31:06 +0000

    Jeffery Zhu and Ji-Hoon Woo's Validations and Unit Tests

---
+++ TestValidation.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+# ------------
+# TestValidation.py
+# ------------
+
+import unittest
+import Validation
+
+# ---------
+# TestAdmin
+# ---------
+
+class TestAdmin (unittest.TestCase) :
+    # ------------
+    # phone_number
+    # ------------
+    def testPhoneNumber1 (self) :
+        self.assert_(not Validation.phone_number('abc'))
+    def testPhoneNumber2 (self) :
+        self.assert_(Validation.phone_number('123-4567'))
+    def testPhoneNumber3 (self) :
+        self.assert_(not Validation.phone_number('11-123-4567'))
+    def testPhoneNumber4 (self) :
+        self.assert_(Validation.phone_number('123-123-4567'))
+    def testPhoneNumber5 (self) :
+        self.assert_(not Validation.phone_number('123-45-67'))
+    def testPhoneNumber6 (self) :
+        self.assert_(not Validation.phone_number('123-4567-1'))
+    # ------------
+    # phone_number_ext
+    # ------------
+    def testphone_number_ext1 (self) :
+        self.assert_(not Validation.phone_number_ext('abc'))
+    def testphone_number_ext2 (self) :
+        self.assert_(Validation.phone_number_ext(''))
+    def testphone_number_ext3 (self) :
+        self.assert_(Validation.phone_number_ext('11'))
+    def testphone_number_ext4 (self) :
+        self.assert_(Validation.phone_number_ext('11111'))
+    # ------------
+    # email
+    # ------------
+    def testemail1 (self) :
+        self.assert_(not Validation.email('abc'))
+    def testemail2 (self) :
+        self.assert_(not Validation.email('abc@11..com'))
+    def testemail3 (self) :
+        self.assert_(not Validation.email('abc..aa@asdf.com'))
+    def testemail4 (self) :
+        self.assert_(Validation.email('abc_asdf@adsfasd.org'))
+    def testemail5 (self) :
+        self.assert_(Validation.email('asdfasdf@cs.asdf.edu'))
+    def testemail6 (self) :
+        self.assert_(Validation.email('abc@asdf.asdf.asdf.asdf.asdf.com'))
+    def testemail7 (self) :
+        self.assert_(not Validation.email('.asdf@sdfsdaf.com'))
+    def testemail8 (self) :
+        self.assert_(not Validation.email('abc.@asdfasd.com'))
+    def testemail9 (self) :
+        self.assert_(Validation.email('-abc@asdfasd.com'))
+    def testemail10 (self) :
+        self.assert_(Validation.email('abc.aaa@asdfasd.com'))
+    def testemail11 (self) :
+        self.assert_(not Validation.email('ab c@asdfasd.com'))
+    def testemail12 (self) :
+        self.assert_(not Validation.email('ab#$c@asdfasd.com'))
+    def testemail13 (self) :
+        self.assert_(not Validation.email('ab@c@asdfasd.com'))
+    # ------------
+    # date
+    # ------------
+    def testdate1 (self) :
+        self.assert_(not Validation.date('00-12-1999'))
+    def testdate2 (self) :
+        self.assert_(not Validation.date('02-30-1999'))
+    def testdate3 (self) :
+        self.assert_(not Validation.date('13-12-1999'))
+    def testdate4 (self) :
+        self.assert_(not Validation.date('01-00-1999'))
+    def testdate5 (self) :
+        self.assert_(Validation.date('02-28-1999'))
+    def testdate6 (self) :
+        self.assert_(not Validation.date('00-42-1999'))
+    def testdate7 (self) :
+        self.assert_(not Validation.date('00-12-99'))
+    def testdate8 (self) :
+        self.assert_(not Validation.date('00-00-12-1999'))
+    def testdate9 (self) :
+        self.assert_(not Validation.date('01-1-1999'))
+    def testdate10 (self) :
+        self.assert_(not Validation.date('5-12-1999'))
+    def testdate11 (self) :
+        self.assert_(not Validation.date('04-31-1999'))
+    def testdate12 (self) :
+        self.assert_(Validation.date('04-30-1999'))
+    # ------------
+    # name
+    # ------------
+    def testname1 (self) :
+        self.assert_(not Validation.name('aaa'))
+    def testname2 (self) :
+        self.assert_(Validation.name('Aaa'))
+    def testname3 (self) :
+        self.assert_(Validation.name('Aaa-dd'))
+    def testname4 (self) :
+        self.assert_(not Validation.name('8aaa'))
+    def testname5 (self) :
+        self.assert_(not Validation.name(''))
+    def testname6 (self) :
+        self.assert_(not Validation.name('Baa--'))
+    def testname7 (self) :
+        self.assert_(not Validation.name('Aa$'))
+    def testname8 (self) :
+        self.assert_(not Validation.name('Ba a'))
+    # ------------
+    # class_number
+    # ------------
+    def testclass_number1 (self) :
+        self.assert_(not Validation.class_number('aaa'))
+    def testclass_number2 (self) :
+        self.assert_(not Validation.class_number('cs'))
+    def testclass_number3 (self) :
+        self.assert_(not Validation.class_number('cs1'))
+    def testclass_number4 (self) :
+        self.assert_(Validation.class_number('cs987'))
+    def testclass_number5 (self) :
+        self.assert_(Validation.class_number('CS987'))
+    def testclass_number6 (self) :
+        self.assert_(not Validation.class_number('cs*76'))
+    def testclass_number7 (self) :
+        self.assert_(not Validation.class_number('Cs333'))
+    def testclass_number8 (self) :
+        self.assert_(not Validation.class_number('cS123'))
+    def testclass_number9 (self) :
+        self.assert_(not Validation.class_number('ee315'))
+    # ------------
+    # number
+    # ------------
+    def testnumber1 (self) :
+        self.assert_(not Validation.number('aaa'))
+    def testnumber2 (self) :
+        self.assert_(not Validation.number(''))
+    def testnumber3 (self) :
+        self.assert_(Validation.number('12312323'))
+
+if __name__ == "__main__" :
+    unittest.main()
---
+++ Validation.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# -------------
+# Validation.py
+# -------------
+
+import re
+
+# ------------
+# phone_number
+# ------------
+def phone_number (s) :
+        return not re.search('^(\d\d\d-)?\d\d\d-\d\d\d\d$', s) is None
+# ----------------
+# phone_number_ext
+# ----------------
+def phone_number_ext (s) :
+        return not re.search('^(\d)*$',s) is None
+# ------------
+# email
+# ------------
+def email (s) :
+        return not re.search('(^(\w|\-|\_)+((\.)?(\w|\-|\_)+)*)\@(\w)+(\-|\w)*(\w)+((\.)?(\w)+(\-|\w)*(\w)+)*\.(com|net|org|gov|edu)$', s) is None
+# ------------
+# date
+# ------------
+def date (s) :
+        result = False
+        #Feb
+        if(not re.search('^(02)\-((0[1-9])|((1|2)(\d)))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+        #Jan, Mar, May...
+        if(not re.search('^(01|03|05|07|08|10|12)\-(30|31|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+        #Apr, Jun, Sep, Nov
+        if(not re.search('^(04|06|09|11)\-(30|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+        return False
+# ------------
+# name
+# ------------
+def name (s) :
+        return not re.search('^[A-Z][a-zA-Z\'\-]*[a-zA-Z]$',s) is None
+# ------------
+# class_number
+# ------------
+def class_number (s) :
+        return not re.search('(^cs\d\d\d)|(^CS\d\d\d)',s) is None
+# ------------
+# number
+# ------------
+def number (s) :
+        return not re.search('^(\d)+$',s) is None
