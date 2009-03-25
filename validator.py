8be8d1bd848a7e5bab403f0c0abf052b62c458e9
jeffery <jeffery@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-03-25 19:16:54 +0000

    Made applicant.html more readable. Started using Validation.py's functions in validator.py
    Jeffery Zhu

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 import re
+import Validation
 
 class Validator:
 	def __init__(self, form_data):
@@ -14,14 +15,30 @@ class Validator:
 			result['key'] = key
 			result['value'] = value
 			ghetto_switch = {
-				'name'     : lambda: True if re.match('.+', value) else False,
+				'name'     : Validation.name(value),
 				'password' : lambda: True if re.match('.{6,30}', value) else False,
 				'address'  : lambda: True if re.match('.+', value) else False,
-				'phone'    : lambda: True if re.match('.+', value) else False,
+				'phone'    : Validation.phone_number(value),
 				# lol, this is ALMOST to RFC 2822, doesn't handle double quoted sections before '@' or square bracket Internet address after, perfect bug op
-				'email'    : lambda: True if re.match('[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?', value) else False,
+				'email'    : Validation.email(value),
 				'comment'  : lambda: True if re.match('.+', value) else False,
 			}[key.split('_')[0]]()
 			result['valid'] = True if ghetto_switch else False
 			self.results.append(result)
+
+#		for key, value in form_data:
+#			result = {}
+#			result['key'] = key
+#			result['value'] = value
+#			ghetto_switch = {
+#				'name'     : lambda: True if re.match('.+', value) else False,
+#				'password' : lambda: True if re.match('.{6,30}', value) else False,
+#				'address'  : lambda: True if re.match('.+', value) else False,
+#				'phone'    : lambda: True if re.match('.+', value) else False,
+#				# lol, this is ALMOST to RFC 2822, doesn't handle double quoted sections before '@' or square bracket Internet address after, perfect bug op
+#				'email'    : lambda: True if re.match('[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?', value) else False,
+#				'comment'  : lambda: True if re.match('.+', value) else False,
+#			}[key.split('_')[0]]()
+#			result['valid'] = True if ghetto_switch else False
+#			self.results.append(result)
 
