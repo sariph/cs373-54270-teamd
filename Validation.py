edbff71a7a335d5bd956343be426e08987d11fbb
ting <ting@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-03-26 03:20:22 +0000

    updated code doc for validator.py, validation.py

--- Validation.py
+++ Validation.py
@@ -1,74 +1,76 @@
 #!/usr/bin/env python
 
-# -------------
-# Validation.py
-# -------------
-
 import re
 
-# ------------
-# phone_number
-# ------------
 def phone_number (s) :
-        return not re.search('^(\d\d\d-)?\d\d\d-\d\d\d\d$', s) is None
-# ----------------
-# phone_number_ext
-# ----------------
+	"""
+	Phone number validator.
+	"""
+	return not re.search('^(\d\d\d-)?\d\d\d-\d\d\d\d$', s) is None
+
 def phone_number_ext (s) :
-        return not re.search('^(\d)*$',s) is None
-# ------------
-# email
-# ------------
+	"""
+	Phone number extension validator.
+	"""
+	return not re.search('^(\d)*$',s) is None
+
 def email (s) :
-        return not re.search('(^(\w|\-|\_)(\+)?((\.)?(\w|\-|\_)+)*)\@(\w)+(\-|\w)*(\w)+((\.)?(\w)+(\-|\w)*(\w)+)*\.(com|net|org|gov|edu|info|biz|us)$', s) is None
-# ------------
-# date
-# ------------
+	"""
+	E-mail address validator.
+	"""
+	return not re.search('(^(\w|\-|\_)(\+)?((\.)?(\w|\-|\_)+)*)\@(\w)+(\-|\w)*(\w)+((\.)?(\w)+(\-|\w)*(\w)+)*\.(com|net|org|gov|edu|info|biz|us)$', s) is None
+
 def date (s) :
-        result = False
-        #Feb
-        if(not re.search('^(02)\-((0[1-9])|((1|2)(\d)))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
-        #Jan, Mar, May...
-        if(not re.search('^(01|03|05|07|08|10|12)\-(30|31|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
-        #Apr, Jun, Sep, Nov
-        if(not re.search('^(04|06|09|11)\-(30|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
-        return False
-# ------------
-# name
-# ------------
+	"""
+	Date validator.
+	"""
+	result = False
+	#Feb
+	if(not re.search('^(02)\-((0[1-9])|((1|2)(\d)))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+	#Jan, Mar, May...
+	if(not re.search('^(01|03|05|07|08|10|12)\-(30|31|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+	#Apr, Jun, Sep, Nov
+	if(not re.search('^(04|06|09|11)\-(30|(((0[1-9])|((1|2)(\d)))))\-[0-9][0-9][0-9][0-9]$',s) is None): return True
+	return False
+
 def name (s) :
-        return not re.search('^^([A-Z]([a-zA-Z]*(\-|\')?[a-zA-Z]*[a-zA-Z](\')?)*(\s)?)+$',s) is None
-# ------------
-# class_number
-# ------------
+	"""
+	Name validator.
+	"""
+	return not re.search('^^([A-Z]([a-zA-Z]*(\-|\')?[a-zA-Z]*[a-zA-Z](\')?)*(\s)?)+$',s) is None
+
 def class_number (s) :
-        return not re.search('(^cs\d\d\d)|(^CS\d\d\d)',s) is None
-# ------------
-# number
-# ------------
+	"""
+	Class number validator.
+	"""
+	return not re.search('(^cs\d\d\d)|(^CS\d\d\d)',s) is None
+
 def number (s) :
-        return not re.search('^(\d)+$',s) is None
+	"""
+	Number validator.
+	"""
+	return not re.search('^(\d)+$',s) is None
 
-# ------------
-# comment
-# ------------
 def comment (s) :
-        return not re.search('.+',s) is None
+	"""
+	Comment validator.
+	"""
+	return not re.search('.+',s) is None
 
-# ------------
-# Degree type
-# ------------
 def degree_type (s) :
-        return not re.search('^(PhD|Masters)$',s) is None
+	"""
+	Degree validator.
+	"""
+	return not re.search('^(PhD|Masters)$',s) is None
 
-# ------------
-# Citizen or Resident
-# ------------
 def citizen (s) :
-        return not re.search('^(citizen|resident)$',s) is None
+	"""
+	Resident/Citizen validator.
+	"""
+	return not re.search('^(citizen|resident)$',s) is None
 
-# ------------
-# Yes or No
-# ------------
 def yes_no (s) :
-        return not re.search('^(yes|no)$',s) is None
+	"""
+	yes/no validator.
+	"""
+	return not re.search('^(yes|no)$',s) is None
--- validator.py
+++ validator.py
@@ -2,23 +2,28 @@ import re
 import Validation
 
 class Validator:
+	"""
+	Generic validator class depending on form type.
+	"""
 	def __init__(self, form_data):
+		"""
+		Constructor validates k-v pairs found in form_data and returns results.
+		"""
 		self.results = []
 
 		#TODO: how would this happen?
-#		if not form_data:
-#			self.valid = False
-#			self.errors.append('You POSTed without data.')
+		#if not form_data:
+			#self.valid = False
+			#self.errors.append('You POSTed without data.')
 
 		for key, value in form_data:
 			result = {}
 			result['key'] = key
 			result['value'] = value
 			ghetto_switch = {
-				
 				#'email'    : lambda: True if re.match('[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?', value) else False,
 				# lol, this is ALMOST to RFC 2822, doesn't handle double quoted sections before '@' or square bracket Internet address after, perfect bug op
-				
+
 				#TA Applicant switches
 				'phone_applicant'		: lambda x: Validation.phone_number(x),
 				'email_applicant'		: lambda x: Validation.email(x),
@@ -32,22 +37,20 @@ class Validator:
 				'comment_programming'		: lambda x: Validation.comment(x),
 				'comment_area'			: lambda x: Validation.comment(x),
 				'comment_qualified'		: lambda x: Validation.comment(x),
-			
+
 				#Instructor Swicthes
 				'comment_wanted'		: lambda x: Validation.comment(x),
 				'comment_unwanted'		: lambda x: Validation.comment(x),
 				'comment_native'		: lambda x: Validation.yes_no(x),
 				'comment_specialization'	: lambda x: Validation.comment(x),
+
 				#Admin switches	
-				
 				'comment_class_name'		: lambda x: Validation.comment(x),
 				'comment_inst_name'		: lambda x: Validation.comment(x),
 				'comment_exp_enrollment'	: lambda x: Validation.number(x),
 				'comment_num_ta_needed'		: lambda x: Validation.number(x),
 				'comment_num_ta_assigned'	: lambda x: Validation.number(x),
-				
-				
 			}[key](value)
-			
+
 			result['valid'] = True if ghetto_switch else False
 			self.results.append(result)
