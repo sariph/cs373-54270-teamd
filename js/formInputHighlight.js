242cb6ece497ffeec5cba8515617f286f06a0a3e
jeffery <jeffery@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-04-03 08:12:08 +0000

    Added support for radio buttons on the Applicant page. Jeffery Zhu

--- js/formInputHighlight.js
+++ js/formInputHighlight.js
@@ -5,7 +5,7 @@ function formInputHighlight() {
 	for(var lcv = 0; lcv < inputs.length; ++lcv) {
 		var input = inputs[lcv];
 		var inputType = input.getAttribute('type');
-		if(inputType == 'text' || inputType == 'password')
+		if(inputType == 'text' || inputType == 'password' || inputType == 'radio')
 			setHighlight(input);
 	}
 }
