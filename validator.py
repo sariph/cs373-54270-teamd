ba4bf30ffc9a4f96c494810ddf2623a742c43a18
jeffery <jeffery@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-04-05 23:02:27 +0000

    fixed validator bug

--- validator.py
+++ validator.py
@@ -20,46 +20,50 @@ class Validator:
 			result = {}
 			result['key'] = key
 			result['value'] = value
-			ghetto_switch = {
-				#'email'    : lambda: True if re.match('[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?', value) else False,
-				# lol, this is ALMOST to RFC 2822, doesn't handle double quoted sections before '@' or square bracket Internet address after, perfect bug op
-
-				'phone'		: lambda x: Validation.phone_number(x),
-				'email'		: lambda x: Validation.email(x),
-				'comment'	: lambda x: Validation.comment(x),
-				'radio'		: lambda x: Validation.comment(x),
-				'number'	: lambda x: Validation.number(x),
-				'unique'	: lambda x: Validation.unique(x),
-				'yesno'		: lambda x: Validation.yes_no(x),
-				'text'		: lambda x: True,
-				'optional'	: lambda x: True,
-				#TA Applicant switches
-				#'phone_applicant'		: lambda x: Validation.phone_number(x),
-				#'email_applicant'		: lambda x: Validation.email(x),
-				#'comment_major'			: lambda x: Validation.comment(x),
-				#'comment_admission'		: lambda x: Validation.date(x),
-				#'comment_phd'			: lambda x: Validation.degree_type(x),
-				#'comment_supervising'		: lambda x: Validation.comment(x),
-				#'comment_citizen'		: lambda x: Validation.citizen(x),
-				#'comment_native'		: lambda x: Validation.yes_no(x),
-				#'comment_ta'			: lambda x: Validation.yes_no(x),
-				#'comment_programming'		: lambda x: Validation.comment(x),
-				#'comment_area'			: lambda x: Validation.comment(x),
-				#'comment_qualified'		: lambda x: Validation.comment(x),
-
-				#Instructor Swicthes
-				#'comment_wanted'		: lambda x: Validation.comment(x),
-				#'comment_unwanted'		: lambda x: Validation.comment(x),
-				#'comment_native'		: lambda x: Validation.yes_no(x),
-				#'comment_specialization'	: lambda x: Validation.comment(x),
-
-				#Admin switches	
-				#'comment_class_name'		: lambda x: Validation.comment(x),
-				#'comment_inst_name'		: lambda x: Validation.comment(x),
-				#'comment_exp_enrollment'	: lambda x: Validation.number(x),
-				#'comment_num_ta_needed'	: lambda x: Validation.number(x),
-				#'comment_num_ta_assigned'	: lambda x: Validation.number(x),
-			}[key.split('_')[0]](value)
+			try:
+				ghetto_switch = {
+					#'email'    : lambda: True if re.match('[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?', value) else False,
+					# lol, this is ALMOST to RFC 2822, doesn't handle double quoted sections before '@' or square bracket Internet address after, perfect bug op
+	
+					'phone'		: lambda x: Validation.phone_number(x),
+					'email'		: lambda x: Validation.email(x),
+					'comment'	: lambda x: Validation.comment(x),
+					'radio'		: lambda x: Validation.comment(x),
+					'number'	: lambda x: Validation.number(x),
+					'unique'	: lambda x: Validation.unique(x),
+					'yesno'		: lambda x: Validation.yes_no(x),
+					'text'		: lambda x: True,
+					'optional'	: lambda x: True,
+					'uniqueUTEID'	: lambda x: Validation.comment(x),
+					#TA Applicant switches
+					#'phone_applicant'		: lambda x: Validation.phone_number(x),
+					#'email_applicant'		: lambda x: Validation.email(x),
+					#'comment_major'			: lambda x: Validation.comment(x),
+					#'comment_admission'		: lambda x: Validation.date(x),
+					#'comment_phd'			: lambda x: Validation.degree_type(x),
+					#'comment_supervising'		: lambda x: Validation.comment(x),
+					#'comment_citizen'		: lambda x: Validation.citizen(x),
+					#'comment_native'		: lambda x: Validation.yes_no(x),
+					#'comment_ta'			: lambda x: Validation.yes_no(x),
+					#'comment_programming'		: lambda x: Validation.comment(x),
+					#'comment_area'			: lambda x: Validation.comment(x),
+					#'comment_qualified'		: lambda x: Validation.comment(x),
+	
+					#Instructor Swicthes
+					#'comment_wanted'		: lambda x: Validation.comment(x),
+					#'comment_unwanted'		: lambda x: Validation.comment(x),
+					#'comment_native'		: lambda x: Validation.yes_no(x),
+					#'comment_specialization'	: lambda x: Validation.comment(x),
+	
+					#Admin switches	
+					#'comment_class_name'		: lambda x: Validation.comment(x),
+					#'comment_inst_name'		: lambda x: Validation.comment(x),
+					#'comment_exp_enrollment'	: lambda x: Validation.number(x),
+					#'comment_num_ta_needed'	: lambda x: Validation.number(x),
+					#'comment_num_ta_assigned'	: lambda x: Validation.number(x),
+				}[key.split('_')[0]](value)
+			except:
+				pass
 
 			result['valid'] = True if ghetto_switch else False
 			self.results.append(result)
