8bb824cf272bcc52511a99793d95ee8a792e0431
estesox <estesox@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-04-18 20:55:06 +0000

    added admin_change_phase.py

---
+++ admin_change_phase.py
@@ -0,0 +1,59 @@
+import os
+import Validation
+
+from TestDataModel import *
+from DataModels import *
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from validator import Validator
+
+class AdminChangePhases(webapp.RequestHandler):
+	"""
+	Class for handling the admin form and validation.
+	"""
+	def __init__(self):
+		"""
+		Constructor initializes results.
+		"""
+		self.phase = db.GqlQuery("SELECT * FROM Phase)
+
+	def get(self):
+		"""
+		Displays the class template upon get request.
+		"""
+		self.template()
+
+	def post(self):
+		"""
+		Validates form elements and will eventually submit the information to a database.
+		"""
+		validator = Validator(self.request.params.items())
+		check = True
+		do_commit = False
+		for result in validator.results:
+			if result['valid'] == False:
+				check = False
+				break
+		if check == True:
+			new_phase = Phase()
+			for result in validator.results:
+				if result['key'] == "comment_phase":
+                                        new_phase.phase = result['value']
+                                        new_phase.put()
+                                        break
+
+		self.results.extend(validator.results)
+		self.template()
+
+	def template(self):
+		"""
+		Renders the template.
+		"""
+		template_values = {
+                        'phase' : self.phase
+		}
+		path = os.path.join(os.path.dirname(__file__), 'templates', 'adminChangePhase.html')
+		self.response.out.write(template.render(path, template_values))
+
+
