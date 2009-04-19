8f83aa7f6adb9efba97b8596a67fc8ce2c931640
levi <levi@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-04-19 00:16:25 +0000

    Finished SMP as far as possible without point system, whoever handled the DataStore gather botched the fields that define point system as they aren't getting transfered, that needs to be fixed before I can move on

--- admin_match.py
+++ admin_match.py
@@ -16,8 +16,8 @@ class AdminMatch(webapp.RequestHandler):
 		"""
 		Constructor initializes results.
 		"""
-		self.applicants = []
-		self.classes = []
+		self.applicants = {}
+		self.classes = {}
 		'''
 		self.courses = [i for i in db.GqlQuery("SELECT * FROM Course")]
 		self.course_classes = None
@@ -32,22 +32,79 @@ class AdminMatch(webapp.RequestHandler):
 		"""
 		Displays the class template upon get request.
 		"""
-		query = db.GqlQuery("SELECT * FROM Applicant")
-		for applicant in query:
+		applicants = db.GqlQuery("SELECT * FROM Applicant")
+		classes = db.GqlQuery("SELECT * FROM Class")
+		for applicant in applicants:
 			user = db.GqlQuery("SELECT * FROM User WHERE UTEID = :1", applicant.UTEID).get()
+#			applicant.__dict__.update(user.__dict__)
 			applicant.first_name = user.first_name
 			applicant.last_name = user.last_name
-			self.applicants.append(applicant)
-			self.response.out.write(vars(applicant))
-		query = db.GqlQuery("SELECT * FROM Class")
-		for lcv in query:
-			self.classes.append(lcv)
+#			applicant.free = True
+			applicant.points = {}
+			for lcv in classes:
+				applicant.points[lcv.key()] = 0
+				if(applicant.specialization_comment == lcv.specialization_comment):
+					applicant.points[lcv.key()] += 1
+				elif(applicant.qualified_comment == lcv.course_id):
+					applicant.points[lcv.key()] += 1
+			self.applicants[applicant.key()] = applicant
+		self.response.out.write(self.applicants)
+		self.response.out.write("<br />\n")
+		for lcv in classes:
+			lcv.assigned = []
+			lcv.free = True
+			lcv.points = {}
+			for applicant in applicants:
+				lcv.points[applicant.key()] = 0
+				if(lcv.native_english == "yes" and applicant.native_english == "no"):
+					lcv.points[applicant.key()] -= 10
+				elif(lcv.unwanted_comment == applicant.UTEID):
+					lcv.points[applicant.key()] -= 100
+				elif(lcv.wanted_comment == applicant.UTEID):
+					lcv.points[applicant.key()] += 100
+				elif(lcv.specialization_comment == applicant.specialization_comment):
+					lcv.points[applicant.key()] += 1
+			self.classes[lcv.key()] = lcv
+		self.response.out.write(self.classes)
+		self.response.out.write("<br />\n")
+		result = self.SMP(self.applicants, self.classes)
 		'''
 		self.applicants = [i for i in db.GqlQuery("SELECT * FROM Course")]
 		for lcv in self.applicants:
 			self.response.out.write(lcv.to_xml())
 		'''
 		self.template()
+
+	def SMP(self, applicants_with_points, classes_with_points):
+		result = {}
+		for key, applicant in applicants_with_points.items():
+			# order tuples (class key, class points) by highest class points
+			for highest_key_points in sorted(applicant.points.items(), self.sortfunc):
+				# if highest class is free
+				if(classes_with_points[highest_key_points[0]].free):
+					# assign class to applicant and set free to False
+					applicant.assigned = highest_key_points[0]
+					applicant.free = False
+					# assign applicant to class and set free to False if enough TAs
+					classes_with_points[highest_key_points[0]].assigned.append(key)
+					if(len(classes_with_points[highest_key_points[0]].assigned) == classes_with_points[highest_key_points[0]].numTA_needed):
+						classes_with_points[highest_key_points[0]].free = False
+					self.response.out.write("Assigning " + applicant.first_name + ' ' + applicant.last_name + " to " + classes_with_points[highest_key_points[0]].class_id)
+				# highest class is not free
+				else:
+					# check if current applicant is better match than any of the existing applicant(s)
+					for existing_applicant_key in classes_with_points[highest_key_points[0]].assigned:
+						if(classes_with_points[highest_key_points[0]].points[key] > classes_with_points[highest_key_points[0]].points[existing_applicant_key]):
+# TODO swap them
+							self.response.out.write("We found a replacement!")
+							break
+						# this else is for debugging, remove it
+						else:
+							self.response.out.write("We found no replacement")
+		return result
+
+	def sortfunc(self, x, y):
+		return cmp(x[1], y[1])
 
 	def post(self):
 		"""
