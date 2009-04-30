5a7d24cf583eba1dc5fbab4ced6bff725f1cfe2e
levi <levi@215de4b6-77b6-11de-adf2-cbd3851e0ccf>
2009-04-30 22:05:26 +0000

    Googlesque search is working, need to make it cover more Models though

--- trunk/admin_search.py
+++ trunk/admin_search.py
@@ -2,6 +2,9 @@ import os
 import data_models
 import data_utilities
 import itertools
+import re
+
+from validator import Validator
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -13,22 +16,44 @@ class AdminSearch(webapp.RequestHandler):
 			system = data_utilities.initialize()
 		self.logged_in = True if system.current_user and system.current_user.class_name() == 'Admin' else False
 
+		self.results = []
+		self.search_results = []
+
 	def get(self):
-		for iter in self.gen_or([data_models.Applicants, data_models.Instructors], ['first_name', 'last_name'], ['Steven', 'Baggs', 'Downing', 'Young']):
-			for result in iter:
-				self.response.out.write(result.first_name + ' ' + result.last_name)
-				self.response.out.write('!\n')
+		self.template()
+
+	def post(self):
+		validator = Validator(self.request.params)
+		self.results.extend(validator.results)
+
+		if(all(map(self.check_valid, self.results))):
+			for result in self.or_query([data_models.Applicants, data_models.Instructors], ['ut_eid', 'first_name', 'last_name'], re.split('\W+', self.request.get('comment|search'))):
+				if(isinstance(result, data_models.Applicants)):
+					result.type = 'Applicants'
+					self.search_results.append(result)
+				elif(isinstance(result, data_models.Instructors)):
+					result.type = 'Instructors'
+					self.search_results.append(result)
 
 		self.template()
 
-	def gen_or(self, classes, fields, items):
-		return itertools.chain((klass.all().filter('%s = ' % field, item) for klass in classes for field in fields for item in items))
+	def or_query(self, classes, fields, items):
+		"""
+		Returns a list of Entities from any of the Models in the classes list, such that there exists a field from fields that matches an item from items
+		"""
+		return dict([(entity.key(), entity) for iter in itertools.chain((klass.all().filter('%s = ' % field, item) for klass in classes for field in fields for item in items)) for entity in iter]).values()
 
 	def template(self):
 		"""
 		Renders the template.
 		"""
-		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'templates', 'adminSearch.html')
+		template_values = {
+			'results': self.results,
+			'search_results': self.search_results
+		}
+		path = os.path.join(os.path.dirname(__file__), 'templates', 'adminSearch.html' if self.logged_in else 'index.html')
 		self.response.out.write(template.render(path, template_values))
+
+	def check_valid(self, dict):
+		return True if dict['valid'] else False
 
